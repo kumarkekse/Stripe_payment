e9c5b1a08084fc07d596d7534956eaf4d7be46ba
kumarkekse <ashishkckse@gmail.com>
2023-02-23 11:30:04 +0530

    Integrate payment functionality.

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,27 @@
+class ChargesController < ApplicationController
+  
+  def create
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+    charge = Stripe::PaymentIntent.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'customer_payment',
+      currency: 'inr'
+    )
+    
+    Stripe::PaymentIntent.retrieve(charge.id)
+
+    Stripe::PaymentIntent.update(charge.id, metadata: {user_id: '3435453'})
+  
+    Stripe::PaymentIntent.confirm(charge.id, {payment_method: 'pm_card_visa'})
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
